3ab47c24a95db819c330cfffb1d7ac15a72f4dc9
David Sliren <bluee.97.berry@gmail.com>
2025-07-31 21:46:06 -0500

    agregue al llave api

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '../public/vite.svg'
 
-$('#app').text('Hello Vite + jQuery!');
+// $('#app').text('Hello Vite + jQuery!');
+const apiKey = import.meta.env.VITE_Api_key;
+console.log('Hello Vite + jQuery!');
